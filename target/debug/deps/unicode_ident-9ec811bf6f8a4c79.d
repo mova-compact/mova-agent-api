3ac13e7dd9c70b5edab38786863bd06f4818badd
Сергей <sergey@example.com>
2026-05-23 08:40:24 +0200

    chore: ignore rust build artifacts

--- target/debug/deps/unicode_ident-9ec811bf6f8a4c79.d
+++
@@ -1,8 +0,0 @@
-D:\Projects_MOVA\mova-agent-api\target\debug\deps\unicode_ident-9ec811bf6f8a4c79.d: C:\cargo_ascii\registry\src\index.crates.io-1949cf8c6b5b557f\unicode-ident-1.0.24\src\lib.rs C:\cargo_ascii\registry\src\index.crates.io-1949cf8c6b5b557f\unicode-ident-1.0.24\src\tables.rs
-
-D:\Projects_MOVA\mova-agent-api\target\debug\deps\libunicode_ident-9ec811bf6f8a4c79.rlib: C:\cargo_ascii\registry\src\index.crates.io-1949cf8c6b5b557f\unicode-ident-1.0.24\src\lib.rs C:\cargo_ascii\registry\src\index.crates.io-1949cf8c6b5b557f\unicode-ident-1.0.24\src\tables.rs
-
-D:\Projects_MOVA\mova-agent-api\target\debug\deps\libunicode_ident-9ec811bf6f8a4c79.rmeta: C:\cargo_ascii\registry\src\index.crates.io-1949cf8c6b5b557f\unicode-ident-1.0.24\src\lib.rs C:\cargo_ascii\registry\src\index.crates.io-1949cf8c6b5b557f\unicode-ident-1.0.24\src\tables.rs
-
-C:\cargo_ascii\registry\src\index.crates.io-1949cf8c6b5b557f\unicode-ident-1.0.24\src\lib.rs:
-C:\cargo_ascii\registry\src\index.crates.io-1949cf8c6b5b557f\unicode-ident-1.0.24\src\tables.rs:
--- target/debug/deps/zmij-86899365fa45b8e6.d
+++
@@ -1,9 +0,0 @@
-D:\Projects_MOVA\mova-agent-api\target\debug\deps\zmij-86899365fa45b8e6.d: C:\cargo_ascii\registry\src\index.crates.io-1949cf8c6b5b557f\zmij-1.0.21\src\lib.rs C:\cargo_ascii\registry\src\index.crates.io-1949cf8c6b5b557f\zmij-1.0.21\src\stdarch_x86.rs C:\cargo_ascii\registry\src\index.crates.io-1949cf8c6b5b557f\zmij-1.0.21\src\traits.rs
-
-D:\Projects_MOVA\mova-agent-api\target\debug\deps\libzmij-86899365fa45b8e6.rlib: C:\cargo_ascii\registry\src\index.crates.io-1949cf8c6b5b557f\zmij-1.0.21\src\lib.rs C:\cargo_ascii\registry\src\index.crates.io-1949cf8c6b5b557f\zmij-1.0.21\src\stdarch_x86.rs C:\cargo_ascii\registry\src\index.crates.io-1949cf8c6b5b557f\zmij-1.0.21\src\traits.rs
-
-D:\Projects_MOVA\mova-agent-api\target\debug\deps\libzmij-86899365fa45b8e6.rmeta: C:\cargo_ascii\registry\src\index.crates.io-1949cf8c6b5b557f\zmij-1.0.21\src\lib.rs C:\cargo_ascii\registry\src\index.crates.io-1949cf8c6b5b557f\zmij-1.0.21\src\stdarch_x86.rs C:\cargo_ascii\registry\src\index.crates.io-1949cf8c6b5b557f\zmij-1.0.21\src\traits.rs
-
-C:\cargo_ascii\registry\src\index.crates.io-1949cf8c6b5b557f\zmij-1.0.21\src\lib.rs:
-C:\cargo_ascii\registry\src\index.crates.io-1949cf8c6b5b557f\zmij-1.0.21\src\stdarch_x86.rs:
-C:\cargo_ascii\registry\src\index.crates.io-1949cf8c6b5b557f\zmij-1.0.21\src\traits.rs:
